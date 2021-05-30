cd3a675ccd87789a60067c1fadf7eae4aed23960
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-05-30 21:32:03 +0100

    adding fuctionality for saving records and JSON formatiing to enable easier viewing of users.json file

---
+++ repositories/users.js
@@ -0,0 +1,47 @@
+const { deepStrictEqual } = require('assert');
+const fs = require('fs');
+class UsersRepository{
+    constructor(filename){
+        if(!filename){
+            throw new Error('Creating a user repository requires a filename')
+        }
+        this.Filename = filename;
+        try {
+            fs.accessSync(this.Filename);
+        } catch (error) {
+            fs.writeFileSync(this.Filename,'[]');
+        }
+    }
+
+    GetAll = async () => {
+        return await JSON.parse(await fs.promises.readFile(this.Filename));
+    }
+
+    Create = async(attrs) => {
+        // Get the latest record
+        const records = await this.GetAll();
+        // push our new record into the existing record
+        records.push(attrs);
+        // Write the updated record to disc
+        await this.WriteAll(records);
+    }
+
+
+
+
+
+
+
+
+    async WriteAll(records) {
+        await fs.promises.writeFile(this.Filename, JSON.stringify(records, null, 2));
+    }
+}
+
+const test = async () => {
+    const repo = new UsersRepository('users.json');
+    await repo.Create({email: 'musa@gmail.com', password: 'mu1234sa'})
+    const data = await repo.GetAll();
+    console.log(data);
+}
+test();
