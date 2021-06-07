97eca22fba7192e2246016c3d58609537313e888
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-06-07 02:08:16 +0100

    Adding authentication for visiting the admin page

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,24 +5,24 @@ const productsRepository = require('../../repositories/products');
 const newProductTemplate = require('../../views/admin/products/new');
 const productListTemplate = require('../../views/admin/products/index');
 const {requireTitle,requirePrice} = require('./validators');
-const {handleErrors}  = require('./middlewares');
+const {handleErrors,requireAuth}  = require('./middlewares');
 
 
 const router = express.Router();
 const upload = multer({storage: multer.memoryStorage()});
 
 
-router.get('/admin/products',async (req,res) => {
+router.get('/admin/products',requireAuth,async (req,res) => {
     const products = await productsRepository.GetAll();
     res.send(productListTemplate({products}));
 });
 
 
-router.get('/admin/products/new',(req,res) => {
+router.get('/admin/products/new',requireAuth,(req,res) => {
     res.send(newProductTemplate({}));
 });
 
-router.post('/admin/products/new',upload.single('image'),[requireTitle,requirePrice],handleErrors(newProductTemplate), async (req,res) => {
+router.post('/admin/products/new',requireAuth, upload.single('image'),[requireTitle,requirePrice],handleErrors(newProductTemplate), async (req,res) => {
     
     const image = req.file.buffer.toString('base64');
     const {title,price} = req.body;
